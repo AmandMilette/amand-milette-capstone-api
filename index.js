4b1c6f4aa2708b19816db463dedb9dbe46b40c6d
AmandMilette <amand.milette@gmail.com>
2025-03-21 17:39:52 -0400

    created post booking api

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bookingRoutes from "./routes/bookingRoutes.js";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.use("/bookings", bookingRoutes);
 
 app.get("/", (req, res) => {
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,13 +1,45 @@
 import express from "express";
 import fs from "fs";
+import { v4 as uuid } from "uuid";
 const router = express.Router();
 
 // get bookings = http://localhost:8000/bookings
 
-router.route("/").get((req, res) => {
-  const bookings = JSON.parse(fs.readFileSync("./data/bookings.json"));
+router
+  .route("/")
+  .get((req, res) => {
+    const bookings = JSON.parse(fs.readFileSync("./data/bookings.json"));
 
-  res.json(bookings);
-});
+    res.json(bookings);
+  })
+  .post((req, res) => {
+    const { pet_name, timestamp, description, owner_name, pet_type, category } =
+      req.body;
+    if (
+      !pet_name.trim() ||
+      !owner_name.trim() ||
+      !description.trim() ||
+      !timestamp.trim() ||
+      !pet_type.trim() ||
+      !category.trim()
+    ) {
+      return res.status(400).send("All fields must be filled out");
+    }
+
+    const bookings = JSON.parse(fs.readFileSync("./data/bookings.json"));
+    const newBooking = {
+      id: uuid(),
+      pet_name,
+      owner_name,
+      description,
+      pet_type,
+      timestamp: new Date(timestamp).getTime(),
+      category,
+    };
+    bookings.push(newBooking);
+
+    fs.writeFileSync("./data/bookings.json", JSON.stringify(bookings));
+    res.status(201).json(newBooking);
+  });
 
 export default router;
